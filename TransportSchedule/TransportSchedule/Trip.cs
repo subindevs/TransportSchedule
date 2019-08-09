b1db10344274dcce0c8cf0d8d6565f015044d516
subindevs <36504048+subindevs@users.noreply.github.com>
2019-08-09 18:30:20 +0530

    added schedules using json

---
+++ TransportSchedule/TransportSchedule/Trip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransportSchedule
+{
+    public class Trip
+    {
+        public Trip()
+        {
+
+        }
+
+        public string from { get; set; }
+        public string to { get; set; }
+        public int  time { get; set; }
+        public int capacity { get; set; }
+        public string vehicle { get; set; }
+        public string notes { get; set; }
+        
+        
+    }
+}
